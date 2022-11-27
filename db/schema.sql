09b8274ff1a47376026cb074c0fd4d6e3330d011
Lara Grocke <laragrocke@Laras-MacBook-Air.local>
2022-11-27 15:06:49 +1030

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS company;
+CREATE DATABASE company;
+
+USE company;
+
+-- Department Table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+
+  PRIMARY KEY (id),
+  UNIQUE KEY (department_name)
+);
+
+-- Role Table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+);
+
+-- Employee Table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT REFERENCES employee(id),
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) 
+    REFERENCES role(id)
+);
